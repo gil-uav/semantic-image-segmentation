7166f7c5fe012529b90a321da64773d8d2628e50
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2020-09-28 16:55:01 +0200

    feat(train.py): added accumulative grad and grad clipping

--- train.py
+++ train.py
@@ -6,67 +6,18 @@ from argparse import ArgumentParser
 import torch
 from dotenv import load_dotenv
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateLogger
 from torch.backends import cudnn
 
 from unet.unet_model import UNet
 
 load_dotenv(verbose=True)
 
-
-def add_program_specific_args(parent_parser):
-    parser = ArgumentParser(
-        parents=[parent_parser],
-        add_help=False,
-        description="Train the UNet on images and target masks",
-    )
-    parser.add_argument(
-        "-dp",
-        "--data-path",
-        metavar="DP",
-        dest="data_path",
-        type=str,
-        default="data" if not os.getenv("DIR_DATA") else os.getenv("DIR_DATA"),
-        help="Load model from a .pth file",
-    )
-    parser.add_argument(
-        "-cdp",
-        "--checkpoint-data-path",
-        metavar="CDP",
-        dest="checkpoints_path",
-        type=str,
-        default="checkpoints/"
-        if not os.getenv("DIR_CHECKPOINTS")
-        else os.getenv("DIR_CHECKPOINTS"),
-        help="Load model from a .pth file",
-    )
-    parser.add_argument(
-        "-valp",
-        "--validation-percent",
-        metavar="VALP",
-        dest="val_percent",
-        type=float,
-        default=10.0 if not os.getenv("VAL_PERC") else os.getenv("VAL_PERC"),
-        help="How much of dataset to be used as validation set.",
-    )
-    parser.add_argument(
-        "-bs",
-        "--batch-size",
-        dest="batch_size",
-        metavar="BS",
-        type=int,
-        default=1 if not os.getenv("BATCH_SIZE") else os.getenv("BATCH_SIZE"),
-        help="Batch size",
-    )
-
-    return parser
-
-
 if __name__ == "__main__":
     logging.getLogger("lightning").setLevel(logging.ERROR)
 
     parser = ArgumentParser()
 
-    parser = add_program_specific_args(parser)
     parser = UNet.add_model_specific_args(parser)
     parser = Trainer.add_argparse_args(parser)
 
@@ -98,12 +49,23 @@ if __name__ == "__main__":
     cudnn.benchmark = True  # cudnn Autotuner
     cudnn.enabled = True  # look for optimal algorithms
 
+    lr_monitor = LearningRateLogger(logging_interval="step")
+    early_stopping = EarlyStopping("val_loss", patience=5, verbose=True)
+
     try:
         trainer = Trainer.from_argparse_args(
             args,
             gpus=-1,
             precision=16,
             distributed_backend="ddp",
+            callbacks=[lr_monitor],
+            early_stop_callback=early_stopping,
+            accumulate_grad_batches=1
+            if not os.getenv("ACC_GRAD")
+            else int(os.getenv("ACC_GRAD")),
+            gradient_clip_val=0.0
+            if not os.getenv("GRAD_CLIP")
+            else float(os.getenv("GRAD_CLIP")),
         )
         trainer.fit(model)
     except KeyboardInterrupt:
